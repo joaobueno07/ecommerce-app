94c53d681462c9752760791dee02e12902f07673
João Bueno <buenotjoao07@gmail.com>
2025-10-02 21:49:10 -0300

    feat: create application, public, components and api folders

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    images: {
+    // definindo o placehold como um dominio seguro na aplicação
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "placehold.co",
+        pathname: "/**",
+      },
+    ],
+  },
+};
 
 export default nextConfig;
